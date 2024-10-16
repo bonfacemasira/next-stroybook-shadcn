7e28d95b1fcad55bc563cee8da1e43dee7cd2d85
bonfacemasira <bonfacemasira@gmail.com>
2024-10-16 10:09:12 +0300

    add Docker and doccker-compose.yml

--- components/ui/button.stories.tsx
+++ components/ui/button.stories.tsx
@@ -11,6 +11,25 @@ const meta: Meta<typeof Button> = {
   parameters: {
     layout: "centered",
   },
+  argTypes: {
+    variant: {
+      control: "select",
+      description: "Button variants",
+      options: [
+        "default",
+        "destructive",
+        "outline",
+        "secondary",
+        "ghost",
+        "link",
+      ],
+    },
+    size: {
+      control: "select",
+      description: "Button sizes",
+      options: ["sm", "md", "lg"],
+    },
+  },
 };
 
 export default meta;
@@ -23,6 +42,60 @@ export const Default: Story = {
     disabled: false,
     onClick: action("Default click"),
     children: "Default button",
+    className: "shadow-lg",
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: "destructive",
+    size: "lg",
+    disabled: false,
+    onClick: action("Destructive button clicked"),
+    children: "Destructive button",
+    className: "shadow-lg",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+    size: "sm",
+    disabled: false,
+    onClick: action("Outline button clicked"),
+    children: "Outline button",
+    className: "rounded-none shadow-lg",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+    size: "sm",
+    disabled: false,
+    onClick: action("Secondary button clicked"),
+    children: "Secondary button",
+    className: "shadow-lg",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+    size: "lg",
+    disabled: false,
+    onClick: action("Ghost button clicked"),
+    children: "Ghost button",
+    className: "shadow-lg",
+  },
+};
+export const Link: Story = {
+  args: {
+    variant: "link",
+    size: "lg",
+    disabled: false,
+    onClick: action("Link button clicked"),
+    children: "Link button",
     className: "shadow-lg",
   },
 };
